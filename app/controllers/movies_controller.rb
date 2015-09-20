26d4f4a6512678890abbaf9d137b8973c0298806
velpulaakshaypaul <velpulaakshaypaul@tamu.edu>
2015-09-20 18:56:12 -0500

    controllertest

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,8 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+ #   id = params[:id] # retrieve movie ID from URI route
+ #   @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 

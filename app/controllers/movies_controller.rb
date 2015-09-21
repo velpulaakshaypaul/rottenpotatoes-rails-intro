ee85830f196b3c4990a518835c30e8643c2fdbe4
velpulaakshaypaul <velpulaakshaypaul@tamu.edu>
2015-09-20 23:20:31 -0500

    removed controller logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,42 +12,7 @@ class MoviesController < ApplicationController
 
   def index
 #    @movies = Movie.all
- redirect = false
-
-        if params[:sort]
-            @sorting = params[:sort]
-        elsif session[:sort]
-            @sorting = session[:sort]
-            redirect = true
-        end
-
-        if params[:ratings]
-            @ratings = params[:ratings]
-        elsif session[:ratings]
-            @ratings = session[:ratings]
-            redirect = true
-        else
-            @all_ratings.each do |rat|
-                (@ratings ||= { })[rat] = 1
-            end
-            redirect = true
-        end
-
-        if redirect
-            redirect_to movies_path(:sort => @sorting, :ratings => @ratings)
-        end
-
-        Movie.find(:all, :order => @sorting ? @sorting : :id).each do |mv|
-            if @ratings.keys.include? mv[:rating]
-                (@movies ||= [ ]) << mv
-            end
-        end
-
-        session[:sort]    = @sorting
-        session[:ratings] = @ratings
-    end
-
-        
+end        
 
   def new
     # default: render 'new' template

3c9309e818a85e498b345aad7879230cfc1ef02a
velpulaakshaypaul <velpulaakshaypaul@tamu.edu>
2015-09-22 11:53:24 -0500

    reveedc all

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-@movies=Movie.all
+#@movies=Movie.all
 #    @movies = Movie.find(:all, :order => (params[:sort_by]))
 #		@sort_column = params[:sort_by]
 #@movies=Movies.order('title')

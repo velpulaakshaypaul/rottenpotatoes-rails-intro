4a9f8ae7b6cef3d308b893e9f45fdf0a18c7d1f8
velpulaakshaypaul <velpulaakshaypaul@tamu.edu>
2015-09-22 14:00:17 -0500

    mo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
-class Movie <  ActiveRecord::Calcuations
+class Movie <  ActiveRecord::Base
 #  attr_accessible :title, :rating, :description, :release_date
 #def self.movies(opt)
 #self.order(opt)
 #def self.all_ratings
  #       self.pluck(:rating).uniq
   #  end
-def self.ratings
- self.pluck(:rating).uniq
-end
+#def self.ratings
+# self.pluck(:rating).uniq
+#end
 end

a389fbca3748893074b5de1cb289ac7b25dd6671
velpulaakshaypaul <velpulaakshaypaul@tamu.edu>
2015-09-22 22:29:38 -0500

    controllerchanges

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,10 +15,10 @@ class MoviesController < ApplicationController
 #atings=params[:ratings] 
 @movies=Movie.movies(params[:ratings].keys , params[:sort_by])
 @ratings=Movie.ratings
-@filters= if params[:ratings].nil?
-@ratings
-else
-params[:ratings]
+#@filters= if params[:ratings].nil?
+#@ratings
+#else
+#params[:ratings]
 end	
 #all_ratings=Movie.all_ratings
 end        
